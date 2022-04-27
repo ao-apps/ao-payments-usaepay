0fc804bd6a80d9c53b51da80de305aa010f2638d
AO Industries, Inc <support@aoindustries.com>
2022-04-26 23:59:51 -0500

    Activated org.openrewrite.java.cleanup.Cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -26,4 +26,4 @@ module com.aoapps.payments.usaepay {
   requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
   requires com.aoapps.payments.api; // <groupId>com.aoapps</groupId><artifactId>ao-payments-api</artifactId>
   requires org.apache.commons.lang3; // <groupId>org.apache.commons</groupId><artifactId>commons-lang3</artifactId>
-}
+} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation

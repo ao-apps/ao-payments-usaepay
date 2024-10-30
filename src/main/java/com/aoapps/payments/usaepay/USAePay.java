8ca7b0daa611186dc34815763aa7328a916addfa
AO Industries, Inc <support@aoindustries.com>
2024-10-30 04:56:45 -0500

    checkstyle-10.19.0 compatibility

--- src/main/java/com/aoapps/payments/usaepay/USAePay.java
+++ src/main/java/com/aoapps/payments/usaepay/USAePay.java
@@ -1,6 +1,6 @@
 /*
  * ao-payments-usaepay - Provider for the USAePay system.
- * Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2008, 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2019, 2020, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -61,18 +61,17 @@ import org.apache.commons.lang3.NotImplementedException;
 
 /**
  * Provider for the USAePay system.
- * <p>
- * Configuration parameters:
- * </p>
+ *
+ * <p>Configuration parameters:</p>
+ *
  * <ol>
  *   <li>providerId - the unique name of the processor</li>
  *   <li>postUrl - the URL used for HTTP POST</li>
  *   <li>key - the source key</li>
  *   <li>pin (optional) - the pin used for MD5 hash transaction verification</li>
  * </ol>
- * <p>
- * TODO: Get this code listed at http://wiki.usaepay.com/developer/3rdparty
- * </p>
+ *
+ * <p>TODO: Get this code listed at http://wiki.usaepay.com/developer/3rdparty</p>
  *
  * @author  AO Industries, Inc.
  */
